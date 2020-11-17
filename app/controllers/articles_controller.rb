260c08347e0e1a65db5acddf13c39a06918508b7
antoniotto71 <antoniotto71@yahoo.it>
2020-11-17 21:38:28 +0100

    Make new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,16 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+  end
+
+  def new
 
   end
+
+  def create
+    render plain: params[:article]
+  end
+
+
+
 end
